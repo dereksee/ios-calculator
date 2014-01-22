a564cfddbfc06a94cb8399d8413717ffdbccf68e
Sahat Yalkabov <sakhat@gmail.com>
2014-01-21 19:56:32 -0500

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Sahat Yalkabov on 1/21/14.
+//  Copyright (c) 2014 Sahat Yalkabov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Sahat Yalkabov on 1/21/14.
+//  Copyright (c) 2014 Sahat Yalkabov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
